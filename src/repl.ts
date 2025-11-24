04794a964f6abf318715c429f4e2ca209b0669de
morauszkia <morauszki.andras@gmail.com>
2025-11-24 23:13:17 +0100

    Add api to state and make functions async

--- src/repl.ts
+++ src/repl.ts
@@ -1,36 +1,41 @@
 import type { State } from "./state.js";
 
 export function cleanInput(input: string): string[] {
-    return input.toLowerCase().trim().split(/\s+/).map(word => word.trim()).filter(word => word !== "");
+  return input
+    .toLowerCase()
+    .trim()
+    .split(/\s+/)
+    .map((word) => word.trim())
+    .filter((word) => word !== "");
 }
 
-export function startREPL(state: State) {
-        const {rl, commands} = state;
-        function evaluateInput(input: string) {
-        const words = cleanInput(input);
-        if (words.length === 0) {
-            rl.prompt();
-            return;
-        }
+export async function startREPL(state: State) {
+  const { rl, commands } = state;
+  async function evaluateInput(input: string) {
+    const words = cleanInput(input);
+    if (words.length === 0) {
+      rl.prompt();
+      return;
+    }
 
-        const commandStr = words[0];
-        const command = commands[commandStr];
+    const commandStr = words[0];
+    const command = commands[commandStr];
 
-        if (!command) {
-            console.log("Unknown command! Type 'help' to list available commands.");
-            rl.prompt();
-            return;
-        }
+    if (!command) {
+      console.log("Unknown command! Type 'help' to list available commands.");
+      rl.prompt();
+      return;
+    }
 
-        try {
-            command.callback(state);
-        } catch (error) {
-            console.log(error)
-        }
-
-        rl.prompt();
+    try {
+      await command.callback(state);
+    } catch (error) {
+      console.log(error);
     }
 
     rl.prompt();
-    rl.on("line", (input) => evaluateInput(input))
+  }
+
+  rl.prompt();
+  rl.on("line", async (input) => await evaluateInput(input));
 }
--- src/state.ts
+++ src/state.ts
@@ -1,26 +1,33 @@
 import { createInterface, type Interface } from "readline";
-import { getCommands } from "./commands.js"
+import { getCommands } from "./commands.js";
+import { PokeAPI } from "./pokeapi.js";
 
 export type CLICommand = {
-    name: string;
-    description: string;
-    callback: (state: State) => void;
-}
+  name: string;
+  description: string;
+  callback: (state: State) => Promise<void>;
+};
 
 export type State = {
-    rl: Interface,
-    commands: Record<string, CLICommand>
-}
+  rl: Interface;
+  commands: Record<string, CLICommand>;
+  api: PokeAPI;
+  nextLocationURL: string | null;
+  prevLocationURL: string | null;
+};
 
 export function initState() {
-    const rl = createInterface({
-        input: process.stdin,
-        output: process.stdout,
-        prompt: "Pokedex > ",
-    })
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+  });
 
-    return {
-        rl,
-        commands: getCommands(),
-    }
+  return {
+    rl,
+    commands: getCommands(),
+    api: new PokeAPI(),
+    nextLocationURL: null,
+    prevLocationURL: null,
+  };
 }
