92c2ebad96f253343e25b4faef7caa5f4fbc08cb
morauszkia <morauszki.andras@gmail.com>
2025-11-24 23:12:50 +0100

    Create PokeAPI class to interface with the web API

---
+++ src/pokeapi.ts
@@ -0,0 +1,22 @@
+export class PokeAPI {
+  private static readonly baseURL = "https://pokeapi.co/api/v2";
+
+  constructor() {}
+
+  async fetchLocations(pageURL?: string): Promise<ShallowLocations> {
+    const fullURL = pageURL || `${PokeAPI.baseURL}/location-area`;
+    const res = await fetch(fullURL);
+    const locationData = await res.json();
+    return locationData;
+  }
+}
+
+export type ShallowLocations = {
+  count: number;
+  next: string;
+  previous: string;
+  results: {
+    name: string;
+    url: string;
+  }[];
+};
